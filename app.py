dc81b58cd74d7f72b24c9238a9bb2340422d8c9a
SSRamiSanta <ssrs_10@hotmail.com>
2022-08-02 02:38:14 -0500

    Autenticacion

--- app.py
+++ app.py
@@ -3,26 +3,26 @@ from routes.usuarios import usuarios
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from models.usuariolog import Usuariolog
+from models.usuario import Usuario
 
 app = Flask(__name__)
 
-app.secret_key= "secret key"
-app.config['SQLALCHEMY_DATABASE_URI']= 'mysql://root:dan961030@localhost/studilydb'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = "secret key"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://sergio:1234@localhost/studilydb"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 SQLAlchemy(app)
-login_manager_app=LoginManager(app)
+login_manager_app = LoginManager(app)
+
 
 @login_manager_app.user_loader
 def load_user(id_Usuario):
-    print(id_Usuario)
-    print('error')
-    return Usuariolog.get_by_id(id_Usuario)
+    user = Usuario.query.filter_by(id_Usuario=id_Usuario).first()
+    nombre = user.nombre_apellido
+    if user:
+        return user
+    else:
+        return None
+
 
 app.register_blueprint(usuarios)
-
-
-
-
-
-
--- routes/usuarios.py
+++ routes/usuarios.py
@@ -5,81 +5,103 @@ from utils.db import db
 from flask_login import login_user, logout_user, login_required
 
 
-
-usuarios = Blueprint('usuarios', __name__)
-
+usuarios = Blueprint("usuarios", __name__)
 
 
-@usuarios.route('/signup')
+@usuarios.route("/signup")
 def signup():
     usuariosdb = Usuario.query.all()
-    return render_template('registro.html', usuariosdb = usuariosdb)
+    return render_template("registro.html", usuariosdb=usuariosdb)
 
-@usuarios.route('/signin', methods=['POST', 'GET'])
+
+@usuarios.route("/signin", methods=["POST", "GET"])
 def signin():
 
-    if request.method=='POST':
-        nusuario = request.form['usuario']
-        contraseña = request.form['contraseña']
-        
-        user=Usuariolog.login(nusuario, contraseña)
-        if user=='Contraseña incorrecta':
+    if request.method == "POST":
+        nusuario = request.form["usuario"]
+        contraseña = request.form["contraseña"]
+
+        user = Usuariolog.login(nusuario, contraseña)
+        if user == "Contraseña incorrecta":
             flash(user)
-            return redirect('/signin')
-        elif user=='Usuario inexistente':
+            return redirect("/signin")
+        elif user == "Usuario inexistente":
             flash(user)
-            return redirect('/signin')
+            return redirect("/signin")
         else:
             login_user(user)
-            return render_template('/principal.html')
-    #else:
-    return render_template('/index.html')
+            return redirect("/principal")
+    # else:
+    return render_template("/index.html")
 
-@usuarios.route('/registro')
+
+@usuarios.route("/registro")
 def dir_registro():
-    return redirect('registro.html')
+    return redirect("registro.html")
 
-@usuarios.route('/obtenerRegistro', methods=['POST'])
+
+@usuarios.route("/obtenerRegistro", methods=["POST"])
 def add_usuario():
 
-    nusuario = request.form['usuario']
-    nombre = request.form['nombre']
-    correo = request.form['correo']
-    contraseña = request.form['contraseña']
+    nusuario = request.form["usuario"]
+    nombre = request.form["nombre"]
+    correo = request.form["correo"]
+    contraseña = request.form["contraseña"]
 
-    user=Usuario.query.filter_by(id_Usuario = request.form['usuario']).first()
+    user = Usuario.query.filter_by(id_Usuario=request.form["usuario"]).first()
     if user:
-        flash('Usuario ya existe', 'warning')
-        return redirect('/signup')
+        flash("Usuario ya existe", "warning")
+        return redirect("/signup")
 
     usuario = Usuario(nusuario, nombre, correo, contraseña)
 
     db.session.add(usuario)
     db.session.commit()
-    
-    flash('usuario creado')
-    return redirect(url_for("usuarios.home"))
+
+    flash("usuario creado")
+    return redirect(url_for("usuarios.signup"))
 
 
-@usuarios.route('/actualizarUsuario/<id>', methods=['POST', 'GET'])
+@usuarios.route("/actualizarUsuario/<id>", methods=["POST", "GET"])
 def act_usuario(id):
     usuario = Usuario.query.get(id)
-    if request.method == 'POST':
-        usuario.id_Usuario = request.form['usuario']
-        usuario.nombre_apellido = request.form['nombre']
-        usuario.correo = request.form['correo']
-        usuario.contraseña = request.form['contraseña'] 
+    if request.method == "POST":
+        usuario.id_Usuario = request.form["usuario"]
+        usuario.nombre_apellido = request.form["nombre"]
+        usuario.correo = request.form["correo"]
+        usuario.contraseña = request.form["contraseña"]
         db.session.commit()
-        
-        return redirect('/registro.html')
-        
-    return render_template('registro.html', usuario=usuario)
 
-@usuarios.route('/borrar/<id>')
+        return redirect("/registro.html")
+
+    return render_template("registro.html", usuario=usuario)
+
+
+@usuarios.route("/borrar/<id>")
 def del_usuario(id):
     usuario = Usuario.query.get(id)
     db.session.delete(usuario)
     db.session.commit()
 
     print(usuario)
-    return redirect('/ingreso')
+    return redirect("/ingreso")
+
+
+@usuarios.route("/principal")
+@login_required
+def principal():
+    return render_template("principal.html")
+
+
+def status_401(error):
+    flash("Acceso denegado")
+    return redirect("/signin")
+
+
+@usuarios.route("/logout")
+def logout():
+    logout_user()
+    return redirect("/signin")
+
+
+usuarios.register_error_handler(401, status_401)
