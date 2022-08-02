b5a60f3ff6845d97030d0ad5fd17124499b8d80e
danirod29 <danielrodf29@gmail.com>
2022-08-01 20:22:32 -0500

    jinja

--- routes/usuarios.py
+++ routes/usuarios.py
@@ -1,17 +1,22 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect
 from models.usuario import Usuario
 from utils.db import db
 
 
 usuarios = Blueprint('usuarios', __name__)
 
-@usuarios.route('/')
+@usuarios.route('/ingreso')
 def home():
+    usuariosdb = Usuario.query.all()
+    return render_template('registro.html', usuariosdb = usuariosdb)
+
+@usuarios.route('/principal')
+def principal():
     return render_template('index.html')
 
 @usuarios.route('/registro')
 def dir_registro():
-    return render_template('registro.html')
+    return redirect('registro.html')
 
 @usuarios.route('/obtenerRegistro', methods=['POST'])
 def add_usuario():
@@ -34,6 +39,11 @@ def add_usuario():
 def act_usuario():
     return 'tarea guardada'
 
-@usuarios.route('/borrar')
-def del_usuario():
-    return 'tarea guardada'
+@usuarios.route('/borrar/<id>')
+def del_usuario(id):
+    usuario = Usuario.query.get(id)
+    db.session.delete(usuario)
+    db.session.commit()
+
+    print(usuario)
+    return redirect('/ingreso')
